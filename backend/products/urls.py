c311004708bf171f7faa672db90b4010ab33b7c8
shabirtech <shabir.husain326@gmail.com>
2023-05-31 19:31:45 +0500

    mixins and generic api view

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     ### url using class base view
 
-    path('', views.product_list_create_view),
+    path('', views.product_mixin_view),
     path('<int:pk>/update/', views.product_update_view),
     path('<int:pk>/delete/', views.product_destroy_view),
-    path('<int:pk>/', views.product_detail_view),
+    path('<int:pk>/', views.product_mixin_view),
 
 
     ### url of function base view 
--- py_client/detail.py
+++ py_client/detail.py
@@ -1,5 +1,5 @@
 import requests
-endpoint = "http://127.0.0.1:8000/api/products/1/"
+endpoint = "http://127.0.0.1:8000/api/products/5/"
 
 get_response = requests.get(endpoint)  ## HTTP  Requests
 print(get_response.json())
