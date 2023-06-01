b30b686955459fddd950293e722dfa88ce7cc715
shabirtech <shabir.husain326@gmail.com>
2023-06-01 11:14:26 +0500

    Token authentication

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .permissions import IsStaffEditorPermission
-
+from api.authentication import TokenAuthentication
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -11,7 +11,7 @@ from .serializers import ProductSerializer
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    authentication_classes = [authentication.SessionAuthentication]
+    authentication_classes = [authentication.SessionAuthentication,TokenAuthentication]
     permission_classes = [permissions.IsAdminUser,IsStaffEditorPermission]
 
     def  perform_create(self, serializer):
