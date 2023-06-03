17f2df21e1e248843354b3f7570fc08cf28ecccd
shabirtech <shabir.husain326@gmail.com>
2023-06-03 23:43:18 +0500

    pagination

--- py_client/list.py
+++ py_client/list.py
@@ -19,4 +19,8 @@ if auth_response.status_code == 200:
     endpoint = "http://127.0.0.1:8000/api/products/"
 
     get_response = requests.get(endpoint, headers=headers) 
-    print(get_response.json())
+    data = get_response.json()
+    next_url = data["next"]
+    results = data['results']
+    print("next_url", next_url)
+    print(results)
